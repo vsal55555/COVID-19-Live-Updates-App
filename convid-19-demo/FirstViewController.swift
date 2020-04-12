9e81d9058339129e86650de9d5cafab892da0fa7
Bishal Shrestha <vsal55555@gmail.com>
2020-04-12 10:26:35 -0700

    Initial Commit

---
+++ convid-19-demo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  convid-19-demo
+//
+//  Created by BSAL-MAC on 4/12/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ convid-19-demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  convid-19-demo
+//
+//  Created by BSAL-MAC on 4/12/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
